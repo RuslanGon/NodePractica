5fdd0ee8076567ddfa36055f60463b148c7f1748
RuslanGon <ruslan441006@gmail.com>
2024-08-26 18:26:33 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ app.use((req, res, next) => {
 res.status(404).send('Oops, Route is not found');
 });
 
+app.use((error, req, res, next) => {
+res.status(500).send(error.massage);
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');

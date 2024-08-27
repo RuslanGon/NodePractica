53f252b0c36910a552d279d0e0db57b34317cdc9
RuslanGon <ruslan441006@gmail.com>
2024-08-27 11:37:32 +0300

    wer

--- src/server.js
+++ src/server.js
@@ -32,6 +32,13 @@ export const startServer = () => {
   app.get('/students/:studentId', async (req, res, next) => {
     const id = req.params.studentId;
     const student = await getStudentById(id);
+    if(!student){
+        res.status(404).json({
+            status: 404,
+            message: `not student with id ${id}`,
+        });
+    }
+
     res.json({
         status: 200,
         message: `successfully get student with id ${id}`,

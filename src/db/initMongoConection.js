d1d1619c3ccae8b0bd9c4b784bb1ff5bc3a5efe8
RuslanGon <ruslan441006@gmail.com>
2024-08-27 10:03:31 +0300

    wer

---
+++ src/db/initMongoConection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { env } from "../utils/env.js";
+
+export const initMongoConection = () => {
+    const conectionLink = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+};
+
+
+
+//
+
+
+

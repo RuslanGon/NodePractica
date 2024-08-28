a1197208a3eece7dfeffd0844c02998e04102bc5
RuslanGon <ruslan441006@gmail.com>
2024-08-28 10:01:00 +0300

    wer

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { env } from './utils/env.js';
 import { ENV_VARS } from './constants/index.js';
 import { notFoundMiddleware } from './middlewares/notFoutdMiddleware.js';
 import { errorhandlerMiddleware } from './middlewares/errorhandlerMiddleware.js';
-import { getAllStudents, getStudentById } from './services/students.js';
+import studentsRouter from './routers/students.js';
 
 export const startServer = () => {
   const app = express();
@@ -20,31 +20,7 @@ export const startServer = () => {
 
   app.use(cors());
 
-  app.get('/students',async (req, res, next) => {
-    const students =  await getAllStudents();
-    res.json({
-        status: 200,
-        message: 'successfully get all students',
-        data: students
-    });
-  });
-
-  app.get('/students/:studentId', async (req, res, next) => {
-    const id = req.params.studentId;
-    const student = await getStudentById(id);
-    if(!student){
-        res.status(404).json({
-            status: 404,
-            message: `not student with id ${id}`,
-        });
-    }
-
-    res.json({
-        status: 200,
-        message: `successfully get student with id ${id}`,
-        data: student
-    });
-  });
+app.use(studentsRouter);
 
   app.use(notFoundMiddleware);
 

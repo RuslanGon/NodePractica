380d106d2ae0037403fbf82d10381edb12ad094d
RuslanGon <ruslan441006@gmail.com>
2024-08-28 11:18:54 +0300

    wer

--- src/middlewares/errorhandlerMiddleware.js
+++ src/middlewares/errorhandlerMiddleware.js
@@ -1,9 +1,18 @@
+import { HttpError } from "http-errors";
+
 export const errorhandlerMiddleware = (error, req, res, next) => {
-    res.status(500).json({
-        status: 500,
-        message: 'Internal server error',
-        data: {
-            message: error.message
-        }
+  if (error instanceof HttpError) {
+    return res.status(error.status).json({
+      status: error.status,
+      message: error.message,
     });
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Internal server error',
+    data: {
+      message: error.message,
+    },
+  });
 };

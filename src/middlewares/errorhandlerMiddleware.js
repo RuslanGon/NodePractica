388bb183b7689aac23ada0059d4fe25002b352ba
RuslanGon <ruslan441006@gmail.com>
2024-08-28 11:26:51 +0300

    wer

--- src/middlewares/errorhandlerMiddleware.js
+++ src/middlewares/errorhandlerMiddleware.js
@@ -1,4 +1,5 @@
 import { HttpError } from "http-errors";
+import { MongooseError } from "mongoose";
 
 export const errorhandlerMiddleware = (error, req, res, next) => {
   if (error instanceof HttpError) {
@@ -6,6 +7,16 @@ export const errorhandlerMiddleware = (error, req, res, next) => {
       status: error.status,
       message: error.message,
     });
+  }
+
+  if(error instanceof MongooseError) {
+    return res.status(500).json({
+        status: 500,
+        message: 'Mongose error',
+        data: {
+         message: error.data,
+        }
+      });
   }
 
   res.status(500).json({

7cd67bb857f33aca82cefb73d6bcf0194fab0a2a
RuslanGon <ruslan441006@gmail.com>
2024-08-29 11:43:31 +0300

    wer

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -2,7 +2,7 @@ import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsync(req.body, { abortEarly: false });
+    await schema.validateAsync(req.body, { abortEarly: false, convert: false });
     next();
   } catch (err) {
     next(createHttpError(400, 'Bad request', {

052635a3d9ee1a74af278990e05f64297ffdd138
RuslanGon <ruslan441006@gmail.com>
2024-08-29 11:40:50 +0300

    wer

--- src/validation/patchStudentSchema.js
+++ src/validation/patchStudentSchema.js
@@ -1,9 +1,24 @@
 import Joi from 'joi';
 
 export const patchStudentSchema = Joi.object({
-    name: Joi.string().min(2).max(20),
-    age: Joi.number().integer().min(7).max(17),
-    gender: Joi.string().valid('male', 'female', 'other'),
-    avgMark: Joi.number().min(1).max(12),
-    onDuty: Joi.boolean(),
+  name: Joi.string().min(2).max(20).messages({
+      'string.min': 'Min string length is not achieved',
+      'string.max': 'Max string length is not achieved',
+    }),
+  age: Joi.number().integer().min(7).max(17).messages({
+      'number.base': 'Age must be a number',
+      'number.min': 'Minimum age is 7',
+      'number.max': 'Maximum age is 17',
+    }),
+  gender: Joi.string() .valid('male', 'female', 'other').messages({
+      'any.only': 'Gender must be one of [male, female, other]',
+    }),
+  avgMark: Joi.number().min(1).max(12).messages({
+      'number.base': 'Average mark must be a number',
+      'number.min': 'Min number length is not achieved',
+      'number.max': 'Max number length is not achieved',
+    }),
+  onDuty: Joi.boolean().messages({
+    'boolean.base': 'OnDuty must be a boolean value',
+  }),
 });

afa58bc1c294767b8a49e543cff2a2ea08cb721b
RuslanGon <ruslan441006@gmail.com>
2024-08-29 10:31:55 +0300

    wer

--- src/middlewares/validateMongoId.js
+++ src/middlewares/validateMongoId.js
@@ -1,8 +1,13 @@
 import createHttpError from 'http-errors';
 import { Types } from 'mongoose';
 
-export const validateMongoId = (req, res, next) => {
-  const id = req.params.studentId;
+export const validateMongoId = (idName = 'id') => (req, res, next) => {
+  const id = req.params[idName];
+
+  if (!id) {
+    throw new Error('id in validateMonqoId is not found');
+  }
+
   if (!Types.ObjectId.isValid(id)) {
     return next(createHttpError(400, 'Invalid id'));
   }
--- src/routers/students.js
+++ src/routers/students.js
@@ -1,24 +1,44 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { createStudentController, deleteStudentController, getStudentByIdController, getStudentsController, patchStudentController, putStudentController } from "../controllers/students.js";
-import { ctrlWrapper } from "../middlewares/ctrlWrapper.js";
+import {
+  createStudentController,
+  deleteStudentController,
+  getStudentByIdController,
+  getStudentsController,
+  patchStudentController,
+  putStudentController,
+} from '../controllers/students.js';
+import { ctrlWrapper } from '../middlewares/ctrlWrapper.js';
+import { validateMongoId } from '../middlewares/validateMongoId.js';
 
 const studentsRouter = Router();
 
 studentsRouter.get('/students', ctrlWrapper(getStudentsController));
 
-studentsRouter.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
+studentsRouter.get(
+  '/students/:studentId',
+  validateMongoId('studentId'),
+  ctrlWrapper(getStudentByIdController),
+);
 
 studentsRouter.post('/students', ctrlWrapper(createStudentController));
 
-studentsRouter.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+studentsRouter.patch(
+  '/students/:studentId',
+  validateMongoId('studentId'),
+  ctrlWrapper(patchStudentController),
+);
 
-studentsRouter.put('/students/:studentId', ctrlWrapper(putStudentController));
+studentsRouter.put(
+  '/students/:studentId',
+  validateMongoId('studentId'),
+  ctrlWrapper(putStudentController),
+);
 
+studentsRouter.delete(
+  '/students/:studentId',
+  validateMongoId('studentId'),
+  ctrlWrapper(deleteStudentController),
+);
 
-studentsRouter.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
-
-
-
-
-  export default studentsRouter;
+export default studentsRouter;

3c68ccf47183f9e41c267187ff9c05009e03a2ca
RuslanGon <ruslan441006@gmail.com>
2024-09-03 19:13:51 +0300

    wer

--- src/utils/googleOAuth.js
+++ src/utils/googleOAuth.js
@@ -1,9 +1,25 @@
 import { OAuth2Client } from "google-auth-library";
+import fs from 'node:fs/promises';
+import path from "node:path";
 import { env } from "./env.js";
 import { ENV_VARS } from "../constants/index.js";
 
-export const client = new OAuth2Client({
+const googleConfig = JSON.parse(
+    fs.readFileSync(path.join(process.cwd(), 'google.json')).toString()
+);
+
+const client = new OAuth2Client({
 clientId: env(ENV_VARS.GOOGLE_CLIENT_ID),
 clientSecret: env(ENV_VARS.GOOGLE_CLIENT_SECRET),
-project_id: ''
+project_id: googleConfig.project_id,
+redirectUri: googleConfig.redirect_uris(0)
   });
+
+export const generateOAuthURL = () => {
+return client.generateAuthUrl({
+    scope: [
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email"
+    ]
+});
+};
